ecbc9e0664ce5da18732899f5e9dd8b25fef7925
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-03-06 23:27:19 +0530

    login page

--- chat-app/src/pages/login.jsx
+++ chat-app/src/pages/login.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Assets from "../assets/assets.js";
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState("Sign up");
   return (
     <div>
       <img src={Assets.logo_big} alt="" />
-      <h2>Signup</h2>
+
       <form action="">
-        <input type="text" placeholder="Username" required />
+        <h2>{currentState}</h2>
+        <div>
+        {currentState == "Sign up" ? (
+          <input type="text" placeholder="Username" required />
+        ) : null}
+        </div>
+        <div>
         <input type="email" placeholder="Email address" required />
+        </div>
+        <div>
         <input type="password" placeholder="Password" required />
+        </div>
+        <div>
         <input type="checkbox" />
         <p>Agree to the terms and privacy policy.</p>
-        <button type="submit">Signup</button>
+        </div>
+        <div>
+        {currentState=="Sign up"?<button type="submit">Create Account</button>
+        : <button type="submit">Login Account</button>}
+        </div>
+        <div>
+            {currentState == "Sign up" ?
         <p>
-          Already have an account <span><a href="">Click here</a></span>
-        </p>
+          Already have an account
+          <span onClick={() => setCurrentState("Login")}>Click here</span>
+        </p>:
+        <p>Create an Account <span onClick={() => setCurrentState("Sign up")}>Click here</span></p>
+        }
         <p>
-          <span><a href="">Forgot Password</a></span>
+          <span>Forgot Password</span>
         </p>
+        </div>
       </form>
     </div>
   );
